50eaebb3b48717cb44753d47be83dfeb51eca8e5
anras5 <anras1filip@gmail.com>
2023-07-22 12:41:15 +0200

    Add dark/light mode toggle

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {ChakraProvider, ColorModeScript} from "@chakra-ui/react";
+import theme from "./theme.js";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    }
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App/>
+        <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+            <RouterProvider router={router}/>
+        </ChakraProvider>
     </React.StrictMode>,
 )
