bcad33e3777458a86e6e8b088fcf15b7794d2028
anras5 <anras1filip@gmail.com>
2023-08-14 10:25:41 +0200

    Fix Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,12 +11,23 @@ const Projects = () => {
     const [getAuth, setAuth, deleteAuth] = useLocalStorage('auth');
     const [projects, setProjects] = useState([]);
     const toast = useToast();
-    const location = useLocation();
+    const toastId = "toast-projects";
+    const [hasLoaded, setHasLoaded] = useState(false);
 
 
     useEffect(() => {
         if (getAuth() !== true) {
             navigate("/");
+            if(!toast.isActive(toastId)) {
+                toast({
+                    id: toastId,
+                    title: 'Sign up or log in first!',
+                    description: "Sign up or log in to browse your projects.",
+                    status: 'warning',
+                    duration: 9000,
+                    isClosable: true,
+                });
+            }
         } else {
             fetch(`http://localhost:8080/api/projects/`, {
                 method: "GET",
@@ -30,6 +41,7 @@ const Projects = () => {
                 return response.json();
             }).then(data => {
                 setProjects(data);
+                setHasLoaded(true);
             }).catch(err => {
                 console.log(err);
                 navigate(0);
@@ -38,25 +50,29 @@ const Projects = () => {
     }, []);
 
     return (
-        <Center>
-            <VStack
-                w={{base: '80%', sm: '90%', md: '75%', lg: '85%', xl: '75%'}}
-                marginBottom={'10'}
-                justify={'center'}
-                spacing={'1rem'}>
-                <Heading
-                    fontWeight={600}
-                    fontSize={{base: '3xl', sm: '4xl', md: '6xl'}}
-                    lineHeight={'110%'}
-                    textAlign={'center'}
-                    py={{base: 10, md: 10}}>
-                    Your projects
-                </Heading>
+        <>
+            {hasLoaded &&
+                <Center>
+                    <VStack
+                        w={{base: '80%', sm: '90%', md: '75%', lg: '85%', xl: '75%'}}
+                        marginBottom={'10'}
+                        justify={'center'}
+                        spacing={'1rem'}>
+                        <Heading
+                            fontWeight={600}
+                            fontSize={{base: '3xl', sm: '4xl', md: '6xl'}}
+                            lineHeight={'110%'}
+                            textAlign={'center'}
+                            py={{base: 10, md: 10}}>
+                            Your projects
+                        </Heading>
 
-                <ProjectsContainer projects={projects}/>
-            </VStack>
+                        <ProjectsContainer projects={projects}/>
+                    </VStack>
 
-        </Center>
+                </Center>
+            }
+        </>
     );
 }
 
