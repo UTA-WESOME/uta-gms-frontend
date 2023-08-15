0272535c6273ba7f1d8335144e30e7ba56a2ad00
Johnybonny <szympast@gmail.com>
2023-08-15 10:46:45 +0200

    Add tempalate for pages with a heading

---
+++ src/components/PageTemplate.jsx
@@ -0,0 +1,29 @@
+import {
+    Center,
+    Heading,
+    VStack,
+} from '@chakra-ui/react';
+
+
+const PageTemplate = (props) => {
+    return (
+        <Center>
+            <VStack
+                w={{ base: '80%', sm: '90%', md: '75%', lg: '85%', xl: '75%' }}
+                marginBottom={'10'}
+                justify={'center'}
+                spacing={'1rem'}>
+                <Heading
+                    fontWeight={600}
+                    fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+                    lineHeight={'90%'}
+                    textAlign={'center'}
+                    py={{ base: 10, md: 10 }}>
+                    {props.title}
+                </Heading>
+                {props.children}
+            </VStack>
+        </Center>
+    );
+};
+export default PageTemplate;
