f59af1608e6183bca164b4562648ee879bbd2dd3
anras5 <anras1filip@gmail.com>
2023-10-11 15:17:46 +0200

    Display graph

---
+++ src/components/project/results-tab/Graphviz.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useMemo } from "react";
+import { graphviz } from "d3-graphviz";
+import { Box } from "@chakra-ui/react";
+
+let counter = 0;
+const getId = () => `graphviz${counter++}`;
+const Graphviz = ({ dot, className }) => {
+    const id = useMemo(getId, []);
+    useEffect(() => {
+        graphviz(`#${id}`, {
+            zoom: false,
+        }).renderDot(dot);
+    }, [dot]);
+
+    return (
+        <Box
+            className={className}
+            id={id}
+            style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+        />
+    )
+
+}
+
+export default Graphviz;
