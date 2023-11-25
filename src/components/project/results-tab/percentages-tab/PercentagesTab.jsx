cafbd5fd60daa3830e04e1dac29405249e013cd1
anras5 <anras1filip@gmail.com>
2023-11-25 14:30:26 +0100

    Code cleanup

--- src/components/project/results-tab/percentages-tab/PercentagesTab.jsx
+++ src/components/project/results-tab/percentages-tab/PercentagesTab.jsx
@@ -1,15 +1,31 @@
 import { Box, Divider, Heading, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 
+const colors = {
+    0: 'teal.900',
+    10: 'teal.800',
+    20: 'teal.700',
+    30: 'teal.600',
+    40: 'teal.500',
+    60: 'teal.400',
+    80: 'teal.300'
+}
+
+const findColor = (number) => {
+    const range = Object.keys(colors).reverse().find(key => number >= parseInt(key));
+    return colors[range];
+}
+
+
 const PercentagesTab = ({alternatives, percentages}) => {
     return (
 
-        <Box textAlign={'center'} mx={{ base: '1%', sm: '5%'}} mt={'10px'}>
+        <Box textAlign={'center'} mx={{ base: '1%', sm: '4%'}} mt={'10px'}>
             <Heading size={{ base: 'md', md: 'xl' }} mb={2}>
                 Percentages
             </Heading>
             <Divider/>
-            <TableContainer mt={3}>
-                <Table variant='striped' size={'sm'}>
+            <TableContainer my={3}>
+                <Table size={'sm'} mb={2}>
                     <Thead>
                         <Tr>
                             <Th>alternative</Th>
@@ -33,7 +49,7 @@ const PercentagesTab = ({alternatives, percentages}) => {
                                     .filter(p => p.alternative === alternative.id)
                                     .sort((x, y) => x.position > y.position ? 1 : x.position < x.position ? -1 : 0)
                                     .map((p, index) => (
-                                        <Td key={index}>
+                                        <Td key={index} bgColor={findColor(p.percent)}>
                                             {p.percent}
                                         </Td>
                                     ))
