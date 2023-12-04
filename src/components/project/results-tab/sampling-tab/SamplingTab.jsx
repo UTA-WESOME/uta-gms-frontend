95b2968bb34a98aafea084ac8879db257b5eb010
anras5 <anras1filip@gmail.com>
2023-12-04 17:26:36 +0100

    Code cleanup

--- src/components/project/results-tab/sampling-tab/SamplingTab.jsx
+++ src/components/project/results-tab/sampling-tab/SamplingTab.jsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Icon, Tab, TabList, TabPanel, TabPanels, Tabs, Text, useMediaQuery } from "@chakra-ui/react";
+import { Icon, Tab, TabList, TabPanel, TabPanels, Tabs, Text, useMediaQuery } from "@chakra-ui/react";
 import { FaRankingStar } from "react-icons/fa6";
 import { GiPodiumWinner } from "react-icons/gi";
 import * as c from "./../../../../config.js";
@@ -9,41 +9,41 @@ const SamplingTab = ({ alternatives, acceptabilityIndices }) => {
     const [isMobile] = useMediaQuery(`(max-width: ${c.Results.Sampling.maxWidthMobile})`);
 
     return (
-            <Tabs
-                variant={'soft-rounded'}
-                colorScheme={'teal'}
-                px={{ base: 1, sm: 5 }}
-                isFitted={isMobile}
-            >
-                <TabList mb={2}>
-                    {isMobile ?
-                        <>
-                            <Tab fontSize={'15px'}>
-                                <Icon as={FaRankingStar}/>
-                            </Tab>
-                            <Tab fontSize={'15px'}>
-                                <Icon as={GiPodiumWinner}/>
-                            </Tab>
-                        </>
-                        :
-                        <>
-                            <Tab>Acceptability indices</Tab>
-                            <Tab>Pairwise winning</Tab>
-                        </>
-                    }
-                </TabList>
-                <TabPanels>
-                    <TabPanel p={1} py={2}>
-                        <AcceptabilityIndicesTab
-                            alternatives={alternatives}
-                            percentages={acceptabilityIndices}
-                        />
-                    </TabPanel>
-                    <TabPanel p={1} py={2}>
-                        <Text>Not implemented yet!</Text>
-                    </TabPanel>
-                </TabPanels>
-            </Tabs>
+        <Tabs
+            variant={'soft-rounded'}
+            colorScheme={'teal'}
+            px={{ base: 1, sm: 5 }}
+            isFitted={isMobile}
+        >
+            <TabList mb={2}>
+                {isMobile ?
+                    <>
+                        <Tab fontSize={'15px'}>
+                            <Icon as={FaRankingStar}/>
+                        </Tab>
+                        <Tab fontSize={'15px'}>
+                            <Icon as={GiPodiumWinner}/>
+                        </Tab>
+                    </>
+                    :
+                    <>
+                        <Tab>Acceptability indices</Tab>
+                        <Tab>Pairwise winning</Tab>
+                    </>
+                }
+            </TabList>
+            <TabPanels>
+                <TabPanel p={1} py={2}>
+                    <AcceptabilityIndicesTab
+                        alternatives={alternatives}
+                        percentages={acceptabilityIndices}
+                    />
+                </TabPanel>
+                <TabPanel p={1} py={2}>
+                    <Text>Not implemented yet!</Text>
+                </TabPanel>
+            </TabPanels>
+        </Tabs>
     )
 }
 
